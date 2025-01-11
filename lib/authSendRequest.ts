db88c0f115405766781982243d85dbb331a3ff77
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2025-01-11 16:17:59 +0900

    added design to magic link email

--- lib/authSendRequest.ts
+++ lib/authSendRequest.ts
@@ -1,5 +1,123 @@
 import { Theme } from "@auth/core/types";
 import { EmailConfig } from "next-auth/providers";
+
+function html(info: {expires: Date, url: string, to:string}) {
+  const {expires, url, to} = info;
+  return `
+  <body style="background: #f9f9f9">
+    <table
+      width="100%"
+      border="0"
+      cellspacing="20"
+      cellpadding="0"
+      style="
+        background: #fff;
+        max-width: 600px;
+        margin: auto;
+        border-radius: 10px;
+      "
+    >
+      <tr>
+        <td align="center">
+          <img
+            src="https://pub-d1a8b5717b5b4146bf4495d7fdd8a0bd.r2.dev/logo.png"
+            style="width: 150px"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td
+          align="center"
+          style="
+            padding: 10px 0px;
+            font-size: 28px;
+            font-family: Helvetica, Arial, sans-serif;
+            color: #444;
+          "
+        >
+          <strong>Let's log you in</strong>
+        </td>
+      </tr>
+      <tr>
+        <td
+          align="center"
+          style="
+            font-size: 16px;
+            line-height: 22px;
+            font-family: Helvetica, Arial, sans-serif;
+            color: #444;
+          "
+        >
+          <p style="display: block; margin: 0">
+            Click the button below to log in to NibbleAI.<br>
+            This link will expire: <br>
+            ${expires}
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" style="padding: 20px 0">
+            <table border="0" cellspacing="0" cellpadding="0" width="80%">
+            <tr>
+              <td align="center" style="border-radius: 5px" bgcolor="#346df1">
+              <a
+                href="${url}"
+                target="_blank"
+                style="
+                font-size: 18px;
+                font-family: Helvetica, Arial, sans-serif;
+                color: #fff;
+                text-decoration: none;
+                border-radius: 5px;
+                padding: 15px 20px;
+                border: 1px solid #346df1;
+                display: inline-block;
+                font-weight:lighter;
+                width: 100%;
+                box-sizing: border-box;
+                "
+                >Sign in</a
+              >
+              </td>
+            </tr>
+            </table>
+        </td>
+      </tr>
+      <tr>
+        <td
+          align="center"
+          style="
+            font-size: 16px;
+            line-height: 22px;
+            font-family: Helvetica, Arial, sans-serif;
+            color: #444;
+          "
+        >
+          <p style="display: block; margin: 0">
+            Confirming this email with securely.<br />
+            log you in using ${to}
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td
+          align="center"
+          style="
+            padding: 0px 0px 10px 0px;
+            font-size: 16px;
+            line-height: 22px;
+            font-family: Helvetica, Arial, sans-serif;
+            color: #979797;
+          "
+        >
+          If you did not request this email you can safely ignore it.
+        </td>
+      </tr>
+    </table>
+  </body>
+  `;
+}
+
 export async function sendVerificationRequest(params:{
     identifier: string;
     url: string;
@@ -9,7 +127,7 @@ export async function sendVerificationRequest(params:{
     theme: Theme;
     request: Request;
 }) {
-  const { identifier: to, provider, url } = params;
+  const { identifier: to, provider, url, expires } = params;
   const { host } = new URL(url);
   const res = await fetch("https://send.api.mailtrap.io/api/send", {
     method: "POST",
@@ -22,8 +140,8 @@ export async function sendVerificationRequest(params:{
       from: {"email":provider.from},
       to: [{"email":to}],
       subject: `Sign in to ${host}`,
-      //   html: html({ url, host, theme }),
-      text: `Please click here to authenticate - ${url}`,
+      html: html({ expires, url, to }),
+      // text: `Please click here to authenticate - ${url}`,
     }),
   });
 
