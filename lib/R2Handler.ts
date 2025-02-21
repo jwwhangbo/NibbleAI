d974799a685b11d0f8a8ca6bf1d333d04b7f8663
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2025-02-21 14:34:07 +0900

    added R2 upload function with progress tracking

--- lib/R2Handler.ts
+++ lib/R2Handler.ts
@@ -1,4 +1,5 @@
 'use server'
+import axios from "axios";
 
 /**
  * Uploads a file to the R2 storage.
@@ -29,6 +30,53 @@ export async function Upload(resource: File, key: string) : Promise<string> {
   });
 
   if (!response.ok) {
+    throw new Error("Failed to upload content");
+  }
+
+  return new URL(key, process.env.NEXT_PUBLIC_R2_AP).toString();
+}
+
+
+/**
+ * Uploads a file to R2 storage and tracks the upload progress.
+ *
+ * @param resource - The file to be uploaded.
+ * @param key - The key (path) where the file will be stored in R2.
+ * @param onProgress - A callback function that receives the upload progress as a percentage.
+ * @returns A promise that resolves to the URL of the uploaded file.
+ * @throws Will throw an error if the R2 worker access point, authentication key, or access point is not set.
+ * @throws Will throw an error if the upload fails.
+ */
+export async function UploadTrackProgress(
+  resource: File,
+  key: string,
+  onProgress: (percentage: number) => void
+): Promise<string> {
+  if (!process.env.R2_WORKER_AP) {
+    throw new Error("R2 worker access point not set");
+  }
+  if (!process.env.R2_AUTH_HEADER_KEY) {
+    throw new Error("failed to find authentication key for r2 storage");
+  }
+  if (!process.env.NEXT_PUBLIC_R2_AP) {
+    throw new Error("failed to find R2 access point");
+  }
+
+  const requestUrl = new URL(key, process.env.R2_WORKER_AP).toString();
+
+  const response = await axios.put(requestUrl, resource, {
+    headers: {
+      "X-Custom-Auth-Key": process.env.R2_AUTH_HEADER_KEY,
+      "Content-Type": resource.type,
+    },
+    onUploadProgress: (progressEvent) => {
+      const { loaded, total } = progressEvent;
+      const percentage = total ? Math.floor((loaded * 100) / total) : 0;
+      onProgress(percentage);
+    },
+  });
+
+  if (response.status !== 200) {
     throw new Error("Failed to upload content");
   }
 
