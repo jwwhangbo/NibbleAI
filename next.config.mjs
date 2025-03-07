0e94b22777cfe104bf5cb897de469918bfae6c21
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2025-03-07 18:29:46 +0900

    fix prod image storage remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.r2.dev",
+        hostname: "storage.nibble-ai.com",
       },
       {
         protocol: "https",
