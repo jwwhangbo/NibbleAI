bb90d9774a3bdb6c471cb79d0643161e162e0910
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2025-03-15 20:16:13 +0900

    Update SideNav.tsx
    Added more links to different pages

--- components/layout/SideNav.tsx
+++ components/layout/SideNav.tsx
@@ -12,26 +12,38 @@ export default function SideNavbar() {
   return (
     <div
       className={clsx([
-        "sm:hidden fixed shadow-inner h-screen w-[247px] bg-white flex-shrink-0 transition-transform duration-75 z-10",
+        "sm:hidden fixed shadow-[inset_0_2px_4px_0_rgba(0,0,0,0.4);] h-screen w-[247px] bg-white flex-shrink-0 transition-transform duration-75 z-10",
         { "-translate-x-[247px]": !active },
       ])}
     >
       {userData ? (
         <UserProfile
-          className="flex justify-start items-center w-full px-4 h-[56px] bg-white border-b-2 m-auto gap-2"
+          className="flex justify-start items-center px-4 h-[56px] border-b-2 m-auto gap-2"
           disabled
           user={userData}
         />
       ) : (
-        <Link href="/auth/signin" className="block font-bold w-full flex justify-center items-center px-4 h-[56px] border-b-2">Sign in</Link>
+        <Link
+          href="/auth/signin"
+          className="block font-bold w-full flex justify-center items-center px-4 h-[56px] border-b-2"
+        >
+          Sign in
+        </Link>
       )}
 
       <ul className="py-2 flex flex-col gap-2 *:mx-2 *:rounded-lg hover:*:bg-gray-200 *:transition *:transitiion-duration-100">
         <li>
-          <Link
-            className="block py-3 px-2"
-            href="/dashboard"
-          >
+          <Link href="/" className="block py-3 px-2">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/category" className="block py-3 px-2">
+            By Category
+          </Link>
+        </li>
+        <li>
+          <Link className="block py-3 px-2" href="/dashboard">
             Dashboard
           </Link>
         </li>
