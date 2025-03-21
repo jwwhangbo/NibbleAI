2f877988c6a7f16c2c84628d2070ffa1707acf6b
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2025-03-21 10:54:04 +0900

    Made visual changes to pagination component

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -13,17 +13,17 @@ export default async function Page({ searchParams } : { searchParams?: Promise<{
   const totalPages = await fetchRecipesPages(query);
 
   return (
-    <div className="w-full px-[17px] mt-[12px]">
-      <div className="block w-full max-w-[800px] mx-auto">
-        <Search
-          className="block w-full"
-          placeholder="Search"
-        />
-        <Suspense key={query + currentPage} fallback={<TableSkeleton/>}>
+    <main className="w-full px-[17px] mt-[12px] mb-2">
+      <div className="block w-full mx-auto">
+        <Search className="block w-full max-w-[800px] mx-auto" placeholder="Search" />
+        <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
           <RecipesTable query={query} currentPage={currentPage} />
         </Suspense>
-        <Pagination className="flex justify-center gap-2 py-4" totalPages={totalPages} />
+        <Pagination
+          className="mt-2 mx-auto w-fit flex justify-center rounded-full py-1 px-2 bg-slate-100"
+          totalPages={totalPages}
+        />
       </div>
-    </div>
+    </main>
   );
 }
