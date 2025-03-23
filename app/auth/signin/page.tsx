14443b390026a7b927963ab0581247a477b1ec9c
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2025-03-23 20:07:20 +0900

    Made some visual changes
    - Added some spacing between components in '/'
    - Made changes to signin page, also fixed text animation

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -2,8 +2,8 @@ import { auth } from "@/auth";
 import Panel from "@/components/signin/SigninPanel";
 import Image from "next/image";
 import { redirect } from "next/navigation";
-import recommendedHero from "@/public/hero_recommended.png"
 import signinBackdrop from "@/public/signin_backdrop.jpg"
+import logoWhite from "@/public/logo_letters_white.png"
 import { Quicksand } from "next/font/google"
 import type { Metadata, ResolvingMetadata } from "next";
 
@@ -48,16 +48,37 @@ export default async function SignInPage(props: {
         <Panel className="m-auto" callbackUrl={callbackUrl} />
       </div>
       <div className="hidden relative lg:flex w-full">
-        <Image src={signinBackdrop} alt="background" fill style={{objectFit:"cover",zIndex:-10, opacity:0.7}}/>
-        <div className="flex flex-col w-full justify-center items-center">
+        <Image
+          src={signinBackdrop}
+          alt="background"
+          fill
+          style={{ objectFit: "cover", zIndex: -10 }}
+        />
+        <div className="flex flex-col w-full justify-center items-center bg-black/50">
           <Image
-            src={recommendedHero}
-            alt="recommended-hero"
-            width={600}
-            height={400}
-            style={{objectFit:"contain"}}
+            src={logoWhite}
+            alt={"logo"}
+            width={100}
+            height={200}
+            style={{ position: "absolute", right: "1rem", top: "1rem" }}
           />
-          <p className={`text-center text-white ${quicksand.className} font-bold text-xl`}>Your Daily Recipes, Generated Just for you</p>
+          <div className="gap-4 flex flex-col">
+            <span
+              className={`text-center text-white ${quicksand.className} text-4xl`}
+            >
+              Try delicious recipes design by AI
+            </span>
+            <span
+              className={`text-center text-white ${quicksand.className} text-2xl`}
+            >
+              Innovation meets incredible flavors
+            </span>
+            <span
+              className={`text-center text-white ${quicksand.className} text-sm`}
+            >
+              Where technology meets taste. Enjoy expertly crafted, AI-generated Recipes
+            </span>
+          </div>
         </div>
       </div>
     </div>
